3ac0929d3bcdf83c8dc421c26ebaedfd1b197a18
Joshua Forest <joshfo@iastate.edu>
2018-02-11 19:48:31 -0600

    added pages

--- ffs-client/src/app/app.module.ts
+++ ffs-client/src/app/app.module.ts
@@ -5,15 +5,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TestingComponent } from './testing/testing.component';
 import { ConnectionTestService } from './connection-test.service';
+import { HomeComponent } from './home/home.component';
+import { RecipesViewerComponent } from './recipes-viewer/recipes-viewer.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestingComponent
+    TestingComponent,
+    HomeComponent,
+    RecipesViewerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [ConnectionTestService],
   bootstrap: [AppComponent]
---
+++ ffs-client/src/app/recipes-viewer/recipes-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-viewer',
+  templateUrl: './recipes-viewer.component.html',
+  styleUrls: ['./recipes-viewer.component.css']
+})
+export class RecipesViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
