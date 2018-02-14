f7c4b2802f283387975c2fc62af2534ecceede1c
Adrian Bolt <ajbolt@iastate.edu>
2018-02-14 09:57:38 -0600

    fixed socketio actually for realz

--- ffs-server/src/test/java/com/example/demo/DemoApplicationTests.java
+++ ffs-server/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.example.demo.conf.TestApplicationConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=com.WS.Application.class)
+@SpringBootTest(classes=TestApplicationConfiguration.class)
 public class DemoApplicationTests {
 
 	@Test
