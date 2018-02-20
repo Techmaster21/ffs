774c67473c5a1ba9c89627bec5b43274d3c8147a
Adrian Bolt <ajbolt@iastate.edu>
2018-02-19 22:22:44 -0600

    the rest

--- ffs-client/src/app/connection-test.service.ts
+++ ffs-client/src/app/connection-test.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ng-socket-io';
+import {RecipeService} from './recipe.service';
+import {Recipe} from './recipe';
+import {Unit} from './unit';
 
 @Injectable()
 export class ConnectionTestService {
-
-  constructor(private socket: Socket) {
+  recipe: Recipe;
+  constructor(private socket: Socket, private recipeService: RecipeService) {
     this.socket.on('connect', () => console.log('successfully connected'));
     this.socket.on('connect_error', () => console.log('connection failed'));
     // this.socket.emit('getRecipe', 1);
     // this.socket.on('getRecipe', (t) => console.log(t.name));
+    this.recipeService.getRecipe(1).subscribe( recipe => {
+        this.recipe = recipe;
+      }
+    );
   }
 
 }
