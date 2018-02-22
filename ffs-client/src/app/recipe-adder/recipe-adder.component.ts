a6c570f12f7c56bab4861e7f0a80b843df90579e
Adrian Bolt <ajbolt@iastate.edu>
2018-02-21 20:31:46 -0600

    Made the recipe thing a little less dumb

--- ffs-client/src/app/recipe-adder/recipe-adder.component.ts
+++ ffs-client/src/app/recipe-adder/recipe-adder.component.ts
@@ -7,11 +7,12 @@ import {FFSer} from '../ffser';
 import {Unit} from '../unit';
 import {MatTableDataSource, MatPaginator} from '@angular/material';
 import {DataSource} from '@angular/cdk/collections';
-import {Observable} from 'rxjs/Rx';
 import {of} from 'rxjs/observable/of';
 import {from} from 'rxjs/observable/from';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-recipe-adder',
@@ -19,8 +20,8 @@ import {Subject} from 'rxjs/Subject';
   styleUrls: ['./recipe-adder.component.css']
 })
 export class RecipeAdderComponent implements OnInit {
-  dataSource: IngredientsSource | null;
-  ingredients = new IngredientDatabase();
+  ingredients = [];
+  dataSource  = new BehaviorSubject<any>(this.ingredients);;
   displayedColumns = ['name', 'quantity', 'unit', 'actions'];
   steps = [];
   units: Unit[];
@@ -31,9 +32,14 @@ export class RecipeAdderComponent implements OnInit {
     if (newIngredient) {
       let ingredient: Ingredient;
       ingredient = {food: {name: newIngredient}, unit: {unitName: newUnits}, quantity: +newQuantity};
-      this.ingredients.addIngredient(ingredient);
+      this.ingredients.push(ingredient);
+      this.dataSource.next(this.ingredients);
       // console.log(this.ingredients);
     }
+  }
+  removeIngredient(index: number) {
+    this.ingredients.splice(index,1);
+    this.dataSource.next(this.ingredients);
   }
   addStep(newStep: string) {
     if (newStep) {
@@ -44,7 +50,7 @@ export class RecipeAdderComponent implements OnInit {
     let recipe: Recipe;
     let cuisine: Cuisine = {name: 'Italian', id: 2};
     let ffser: FFSer = {ffser: 3};
-    recipe = {name: name, ingredients: this.ingredients.data, description: description, steps: this.steps, cuisine: cuisine,
+    recipe = {name: name, ingredients: this.ingredients, description: description, steps: this.steps, cuisine: cuisine,
       ffser: ffser};
     console.log(recipe);
     this.recipeService.saveRecipe(recipe);
@@ -55,44 +61,6 @@ export class RecipeAdderComponent implements OnInit {
       this.units = units;
       console.log(units);
     });
-    this.dataSource = new IngredientsSource(this.ingredients);
   }
 
-}
-
-export class IngredientDatabase {
-  /** Stream that emits whenever the data has been modified. */
-  dataChange: BehaviorSubject<Ingredient[]>  = new BehaviorSubject<Ingredient[]>([]);
-  get data(): Ingredient[] { return this.dataChange.value; }
-
-  constructor() {
-  }
-
-  /** Adds a new ingredient to the database. */
-  addIngredient(ingredient) {
-    const copiedData = this.data.slice();
-    copiedData.push(ingredient);
-    this.dataChange.next(copiedData);
-  }
-
-  removeIngredient(index: number) {
-    const copiedData = this.data.slice();
-    copiedData.splice(index, 1);
-    this.dataChange.next(copiedData);
-  }
-
-}
-
-
-export class IngredientsSource extends DataSource<any> {
-  constructor(private ingredientDatabase: IngredientDatabase) {
-    super();
-  }
-
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<Ingredient[]> {
-    return this.ingredientDatabase.dataChange;
-  }
-
-  disconnect() {}
 }
