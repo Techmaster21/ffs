638467bcdf38c19f053d3a091443147883a784bd
Samuel Jorgensen <sdj@iastate.edu>
2018-03-01 13:17:28 -0600

    Added CRUDS for (almost) all controllers

--- src/main/java/com/WS/Controllers/CuisineController.java
+++ src/main/java/com/WS/Controllers/CuisineController.java
@@ -5,12 +5,19 @@
  */
 package com.WS.Controllers;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.WS.Entity.Cuisine;
+import com.WS.Repository.CuisineRepository;
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.OnEvent;
 
 /**
  *
@@ -18,6 +25,7 @@ import com.corundumstudio.socketio.SocketIOServer;
  */
 @Component
 public class CuisineController {
+	private CuisineRepository cuisineRepository;
     private final SocketIOServer server;
     private final Logger logger = LoggerFactory.getLogger(RecipeController.class);
 
@@ -29,4 +37,26 @@ public class CuisineController {
     public CuisineController(SocketIOServer server) {
         this.server = server;
     }
+    
+    @OnEvent(value = "getAllCuisines")
+    public void getAllCuisines(SocketIOClient client, AckRequest request, Integer data) {
+        List<Cuisine> cuisines = (List<Cuisine>) cuisineRepository.findAll();
+        client.sendEvent("getAllCuisines", cuisines);
+    }
+    
+    @OnEvent(value = "getCuisine")
+    public void getCuisine(SocketIOClient client, AckRequest request, Integer data) {
+    	client.sendEvent("getCuisine", cuisineRepository.findOne(data));
+    }
+    
+    @OnEvent(value = "deleteCuisine")
+    public void deleteCuisine(SocketIOClient client, AckRequest request, Integer data){
+    	cuisineRepository.delete(data);
+    }
+    
+    @OnEvent(value = "saveCuisine")
+    public void saveCuisine(SocketIOClient client, AckRequest request, Cuisine data){
+        cuisineRepository.save(data);
+    }
 }
+
