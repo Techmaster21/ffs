083e0cd0a6a9843733354cd5a68e46d2747d1f64
Adrian Bolt <ajbolt@iastate.edu>
2018-03-26 22:33:04 -0500

    fixed white error page thing

--- src/main/java/com/WS/Application.java
+++ src/main/java/com/WS/Application.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
@@ -26,12 +27,11 @@ public class Application {
      * (html, css, js, etc) paths to the root path (/) so that Angular can take it from there.
      * @return
      */
-    /*
     @GetMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/";
     }
-*/
+    
     /**
      * Creates and configures the socket.io server. 
      * @return
