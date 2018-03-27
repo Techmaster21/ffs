fc1af3f7f0a45f7a38dea2b1a1a95be95ea0c12f
Adrian Bolt <ajbolt@iastate.edu>
2018-03-27 10:58:27 -0500

    Fixed some minor frontend compile errors

--- ffs-client/src/app/components/home/home.component.ts
+++ ffs-client/src/app/components/home/home.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../../services/account.service';
 import { Router } from '@angular/router';
+import { FFSer } from '../../models/ffser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor(protected accountService: AccountService) {
+export class HomeComponent implements OnInit {
+  user: string;
+  constructor(private accountService: AccountService) {
+  }
+  ngOnInit() {
+    this.user = this.accountService.getUser();
   }
 }
