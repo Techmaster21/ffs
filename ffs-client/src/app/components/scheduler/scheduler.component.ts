ba13668adc8fd5b21046de77d1ada515f9bca325
Adrian Bolt <ajbolt@iastate.edu>
2018-04-17 17:00:35 -0500

    Added functionality to date and time picker in scheduler, calculates start and end time

--- ffs-client/src/app/components/scheduler/scheduler.component.ts
+++ ffs-client/src/app/components/scheduler/scheduler.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Recipe } from '../../models/recipe';
-
+import * as moment from 'moment';
+import { Duration, Moment } from 'moment';
 
 @Component({
   selector: 'app-scheduler',
@@ -9,12 +10,24 @@ import { Recipe } from '../../models/recipe';
 })
 export class SchedulerComponent {
   selectedRecipe: Recipe;
-  variable = "18:00";
+  date: Date;
+
   recipeSelect(recipe: Recipe): void {
     this.selectedRecipe = recipe;
   }
-  addEvent(): void {
 
+  addEvent(time: string): void {
+    const hours = +time.split(':')[0];
+    const minutes = +time.split(':')[1];
+    const prepTime: Duration = moment.duration(this.selectedRecipe.prepTime);
+    const cookTime: Duration = moment.duration(this.selectedRecipe.cookTime);
+    const momentDate: Moment = moment(this.date);
+    const start: Date = momentDate.add(hours, 'hours')
+      .add(minutes, 'minutes')
+      .toDate();
+    const end: Date = momentDate.add(prepTime)
+      .add(cookTime)
+      .toDate();
   }
 
 }
